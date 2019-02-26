5979fd8612a0c98809eb9ddc35b781d162926995
Joel Castillo Guerra <joelcastillog@gmail.com>
2019-02-26 16:33:06 +0100

    3

---
+++ KwetterApp/src/main/java/com/joel/KwetterApp/KwetterAppApplication.java
@@ -0,0 +1,13 @@
+package com.joel.KwetterApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KwetterAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KwetterAppApplication.class, args);
+	}
+
+}
